7644364ee23623662926693b1b8bc27586cbb0c4
U-ZENMEN-caonannan <caonannan@zenmen.com>
2017-04-01 18:13:49 +0800

    finished write and read test

--- src/main/test_put.cc
+++ src/main/test_put.cc
@@ -17,17 +17,56 @@ using namespace std;
 
 #define MAX_HOST_SIZE 1024
 #define MAX_KEY_STR_SIZE 1024
+#define MAX_SET_SIZE 100
+#define MAX_NAMESPACE_SIZE 100
 
-char aeroNameSpace[MAX_NAMESPACE_SIZE];
-char aeroSet[MAX_SET_SIZE];
-char aeroHost[MAX_HOST_SIZE];
-int aeroPort;
+char aeroNameSpace[MAX_NAMESPACE_SIZE] = "keyStore";
+char aeroSet[MAX_SET_SIZE] = "testStore";
+char aeroHost[MAX_HOST_SIZE] = "127.0.0.1";
+char aeroKeyStr[MAX_KEY_STR_SIZE] = "aeroTestKey";
+int aeroPort = 3000;
+as_key aeroKey;
 
 void connectToAerospike( aerospike & as ); 
+void dumpRecord( as_record & pRec );
+void initKey( as_key & key );
 
 int main( int argc, char * argc [] ) { 
     aerospike as;
-    connectToAerospike( &as );
+    connectToAerospike( &as ); //first connect to aerospike
+    as_error err; 
+
+    as_record record; //create record 
+    as_record_init( &record, 2 );
+    as_record_set_int64( &record, "bin1", 1234 );
+    as_record_set_str( &record, "bin2", "bin2-data");
+
+    dumpRecord( &record );
+
+    initKey( &aeroKey );
+
+    if( aerospike_key_put( &as, &err, NULL, aeroKey, &record ) != AEROSPIKE_OK ) {
+        cout << "there is some problems, I can not put record into aerospike!" << endl;
+        aerospikeCleanUp(& as);
+        exit(-1);
+    }
+
+    as_record * pRec = NULL;
+    if( aerospike_key_get( &as, &err, NULL, aeroKey, &pRec ) != AEROSPIKE_OK ) {
+        cout << "there is some problems, I can not read record from aerospike!" << endl;
+        aerospikeCleanUp( &as );
+        exit( -1 );
+    }
+
+    dumpRecord( pRec );
+
+    as_record_destroy( pRec );
+
+    return 0;
+}
+
+void initKey() {
+    as_key_init_str( &aeroKey, aeroNameSpace, aeroSet, aeroKeyStr );
 }
 
 void connectToAerospike( aerospike & tas ) {
@@ -48,4 +87,36 @@ void connectToAerospike( aerospike & tas ) {
         exit( -1 );
     }
 
+}
+
+void dumpRecord( const as_record * pRec ) {
+    if( ! pRec ) {
+        cout << "The pointer is NULL" << endl;
+        return;
+    }
+
+    if( pRec->key.valuep ) {
+        char * keyValueStr = as_val_tostring( pRec -> key.valuep );
+        cout << "Key : " << keyValueStr << endl;
+        free( keyValueStr );
+    }
+
+    uint16_t numBins = as_record_numbins( pRec );
+
+    cout << "Generation :" << pRec->gen << " TTL: " << pRec-ttl << " NumBins: " << numBins << endl;
+
+    as_record_iterator iter;
+    as_record_iterator_init( &iter, pRec );
+    while( as_record_iterator_has_next( &iter ) ) {
+        as_bin * pBin = as_record_iterator_next( &iter );
+        if( ! pBin ) {
+            cout << "The bin is null " << endl;
+        } else {
+            char * valAsStr = as_val_tostring( as_bin_get_value( pBin ) );
+            cout << "Bin Name : " << as_bin_get_name( pBin) << " Bin Value: " << valAsStr << endl;
+            free( valAsStr );
+        }
+    }
+
+    as_record_iterator_destroy( &iter );
 }
